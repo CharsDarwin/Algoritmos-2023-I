33d1f36b36e2aefbe37e3e7d50a8bf4c685cab1a
CharsDarwin <140976145+CharsDarwin@users.noreply.github.com>
2023-08-04 20:54:35 -0500

    04.08.23

---
+++ Ejercicio3.py
@@ -0,0 +1,31 @@
+#crear una opcion de menu 
+#1->saludar
+#2->sumar
+#3->salir
+n=True
+opc=0
+nota=[]
+while(n):
+    print("\n1. ingresar notas")
+    print("2. calcular notas ")
+    print("3. salir")
+    opc=int(input("Digite Obcion : "))
+    if(opc==1):
+        while()
+            
+            nota.append=float(print("Digite nota: ");
+            
+            
+        n=True
+    if(opc==2):
+        num1=int(input("Digite a : "))
+        num2=int(input("Digite b : "))
+        
+        print("la suma es : ",num1+num2)
+        n=True
+    if(opc==3):
+        n=False
+    else:
+        print("datos no reconocidos")
+
+
